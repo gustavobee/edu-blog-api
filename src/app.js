baf40716e8ec0350081f9db686d89cf1a55c9d3b
gustavobee <gustavobee0@gmail.com>
2026-09-14 22:18:54 -0300

    fix(api): adiciona middleware de CORS para integracao com frontend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/database');
 const postRoutes = require('./routes/postRoutes'); 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(postRoutes);
